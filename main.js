ac218c41cc9139983c0f0994de95ad2b80114950
Mahliyo <gasandigital13@gmail.com>
2026-09-07 16:30:14 +0500

    Javascript 1-dars star let alert log prompit

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ console.log(x);
 
 let ism = prompt("ismingizni kiriting ");
 let yosh =prompt("yoshingizni kriting");
-let c = ism + "yoshi:" + yosh;
+let c = ism + "," "yoshi:" + yosh;
 console.log(c);
 
