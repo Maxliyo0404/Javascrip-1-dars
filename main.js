39b132ff618a850844095ad45d4aed7b55878586
Mahliyo <gasandigital13@gmail.com>
2026-09-11 18:25:32 +0500

    Primitive va noPrimitive

--- main.js
+++ main.js
@@ -12,15 +12,15 @@
 //   console.log(yil);
 
 
-// Number
+//  Number
 
-// const PI = 3.14;
-// console.log(PI);
+//  const PI = 3.14;
+//   console.log(PI);
 
 // String:
   
-// const ism = "Nargiza";
-// console.log(ism);
+//  const ism = "Nargiza";
+//  console.log(ism);
 
 
 //  Boolean;
@@ -32,8 +32,8 @@
 //  }
 
 // Undefined; Qiymat berilmagan:
-// let x ;
-// console.log(x);
+//  let x ;
+//  console.log(x);
 
 
  // Null;  Bo'sh ;
@@ -41,9 +41,8 @@
 //  console.log(bosh);
  
 //  BigInt: Juda katta butun sonlar uchun ;
-// const kattaSon = 2345623456789098765432323456789009876543222345n;
-// console.log(kattaSon);
-
+//  const kattaSon = 2345623456789098765432323456789009876543222345n;
+//  console.log(kattaSon)
 // Symbol; Noyob va o'zgarmas idinfikator yaratish uchun;
 // const id = Symbol("id");
 // console.log(id );
@@ -61,11 +60,124 @@
 
 // console.log(salomBer("Maxliyo"));
 
-// Object
+// // Object
 
-const user = {
-  id:1,
-  name: "Ali",
-  hobby:"sleep"
-}
-console.log(user);
+// const user = {
+//   id:1,
+//   name: "Ali",
+//   hobby:"sleep"
+// }
+// console.log(user);
+
+
+
+
+
+// const yil = 2010;
+//  yil = 2011;
+// console.log(yil);
+
+//  const yil = 2010;
+//  console.log(yil);
+
+//  let name = "ibrohim";
+//  console.log(name)
+//  ;
+
+
+
+//  let yosh = 16 ;
+//  if(yosh < 16){
+//     alert("true")
+//  } else{
+//     alert("false")
+
+//  } 
+ 
+ 
+
+// var shakar = 1600;
+// var shakar = 1700;
+// console.log(shakar);
+ 
+
+// const ism = "Malika";
+// ism = "Ali";
+// console.log(ism);
+
+// 1. Primitive  sodda tur
+// 1. Number
+
+// let PI = 3.14;
+// console.log(PI);
+
+// 2. sTRING;
+
+// let ism = "Sarvinoz";
+// console.log(ism);
+
+//3. Undifined;
+ 
+// let x ;
+// console.log(x);
+
+//4. Null;
+// let olma = null;
+// console.log(olma);
+
+
+//5 Boolean;
+
+// let  yosh = 14;
+ 
+// if (yosh >= 18) {
+//     alert("true");
+// }
+// else{
+//     alert("false");
+
+// 6. BigInt;
+// let son = 112345678909876543212345678909876543n;
+// console.log(son);
+//  //7. Symbol
+//   let id = Symbol("id");
+//   console.log(id);
+
+// //1.number
+
+// let w = 34
+// console.log(w);
+
+// //2.string
+// let s = "yasmina"
+// console.log(s);
+
+
+// //3.undefined
+
+// let u 
+// console.log(u);
+
+// //4.null
+
+// let telefon = null
+// console.log(telefon);
+
+// //5.boolene
+// let pamidor  = 14.000
+// if (pamidor >= 15.000) {
+//     alert("true");
+// }
+// else{
+//     alert("false");
+// }
+
+// //6.begint
+// let son = 123456789012345678998765432;
+// console.log(son);
+
+// //7.symbal
+// let pasport  = Symbol("pasport");
+// console.log(pasport);
+
+
