8ac041538052f980572cf51f3bf8f35071129f19
Mahliyo <gasandigital13@gmail.com>
2026-09-17 07:24:48 +0500

    javascriptda barcha operatorlar && and va

--- main.js
+++ main.js
@@ -352,7 +352,7 @@ console.log(30 <= 30 );
    
 //  || -or - yoki
 
-Uyni eshigini ochish kerak:  Kalit : qizim va o'limda bor
+// Uyni eshigini ochish kerak:  Kalit : qizim va o'limda bor
 
 console.log();
 
