5432f211b0916c12d016b05d6f705ffffff3117d
Mahliyo <gasandigital13@gmail.com>
2026-09-17 07:27:27 +0500

    javascriptda barcha operatorlar && and va

--- main.js
+++ main.js
@@ -356,7 +356,7 @@ console.log(30 <= 30 );
             //  qizim    o'g'lim
 console.log( true || false);
             //  qizim    o'g'lim
-console.log( true || false);
+console.log( false || false);
 
 
 // ! not emas
